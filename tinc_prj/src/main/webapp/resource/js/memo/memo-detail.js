d750363d632b008a2d86351537b327b2d92958d2
Randy Kim <kimho314@gamil.com>
2020-01-03 23:03:04 +0900

    change memo pages' layout design delete dummy files

--- tinc_prj/src/main/webapp/resource/js/memo/memo-detail.js
+++ tinc_prj/src/main/webapp/resource/js/memo/memo-detail.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function () {
 
-    function cmaTextareaSize(bsize) { // 객체명, 기본사이즈
+    function cmaTextareaSize(bsize) { // 기본사이즈
         var sTextarea = document.querySelector(".memo-detail-content-textarea");
         var csize = (sTextarea.scrollHeight >= bsize) ? sTextarea.scrollHeight + "px" : bsize + "px";
         sTextarea.style.height = bsize + "px";
@@ -11,7 +11,5 @@ window.addEventListener("load", function () {
     var contentTextAreaHeight = memoDetailContentTextArea.getBoundingClientRect().height;
 
     cmaTextareaSize(contentTextAreaHeight);
-
-
 
 });
