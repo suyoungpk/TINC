d45813ff4c745883d16471083c3951e54eb6ca5b
Randy Kim <kimho314@gamil.com>
2020-01-06 16:57:36 +0900

    add dependencies for Transactional annotation change menu due-date layout

--- tinc_prj/src/main/webapp/resource/js/memo/memo-checklist.js
+++ tinc_prj/src/main/webapp/resource/js/memo/memo-checklist.js
@@ -1,15 +1,11 @@
 window.addEventListener("load", function () {
     var checkListItemList = document.querySelectorAll(".memo-checklist-item-list");
 
-    // this.console.log(checkListItemList.length);
-    // this.console.log(checkListItemList[0].children.length);
+
     for (var i = 0; i < checkListItemList.length; i++) {
 
-
         for (var j = 0; j < checkListItemList[i].children.length; j++) {
-            this.console.log(checkListItemList[i].children[j].children[0]);
-            this.console.log(checkListItemList[i].children[j].children[1]);
-            this.console.log(checkListItemList[i].children[j].children[2]);
+
             checkListItemList[i].children[j].children[0].id = "checklist-item-checkbox" + i + "-" + j;
             checkListItemList[i].children[j].children[1].setAttribute("for", checkListItemList[i].children[j].children[0].id);
         }
