c7e20f97c7d56b19127a7bc0a76f2d8879623735
Eunsun-Lee <55965932+Eunsun-Lee@users.noreply.github.com>
2020-01-10 18:08:53 +0900

    setting 경로 및 파일 수정

--- tinc_prj/src/main/webapp/resource/js/setting/setting.js
+++ tinc_prj/src/main/webapp/resource/js/setting/setting.js
@@ -1,5 +1,13 @@
-$(function(){ $("#setting-wrapper").load("setting.html"); });
+// --- popup 뒤 내용 보이기
+$(function(){ 
+    $("#setting-wrapper").load("setting.html");
+    
+    $(".setting-close").click(function(){
+        location.href = 'setting.html';
+    })
+});
 
+// ------ 프로필 사진 변경 ------ 미완
 $(function() {
     $("#profile-img").on("change", function(){
         var files = !!this.files ? this.files : [];
@@ -12,4 +20,77 @@ $(function() {
             };
         }
     });
+});
+
+// ------ setting ------
+$(function(){
+    $("#setting-edit-pwd").click(function(e){
+        location.href='change-pwd.html';
+        e.preventDefault();
+        e.stopPropagation();
+    });
+
+    $("#ok-edit-pwd").click(function(e){
+        location.href = 'setting.html';
+        e.preventDefault();
+        e.stopPropagation();
+    });
+
+    $("#close-change-pwd").click(function(e){
+        location.href = 'setting.html';
+        e.preventDefault();
+        e.stopPropagation();
+    });
+
+    $("#setting-secession").click(function (e) { 
+        location.href='withdraw.html';
+        e.preventDefault();
+        e.stopPropagation();
+    });
+
+    $("#set-logout").click(function(){
+        console.log("로그아웃");
+        location.href = 'logout.html';
+    });
+});
+// ------ withdraw ------
+$(function(){
+    $("#withdraw-ok1").click(function(){
+        $("#withdraw-ask1").hide();
+        $("#withdraw-ask2").css("display", "block");
+    });
+
+    $("#withdraw-cancle1, #withdraw-cancle2").click(function(e){
+        location.href = 'setting.html';
+        e.preventDefault();
+        e.stopPropagation();
+    });
+
+    $("#withdraw-ok2").click(function(e){
+        location.href = '../member/main.html';
+        e.preventDefault();
+        e.stopPropagation();
+    });
+});
+
+// ------ logout ------
+$(function(){
+    $("#logout-cancle").click(function () { 
+        history.go(-1);
+    });
+    $("#logout-ok").click(function (e) { 
+        location.href = '../member/main.html';
+        e.preventDefault();
+        e.stopPropagation();
+    });
+});
+
+// ------ change-pwd ------
+$(function(){
+    $("#cancle-edit-pwd, #ok-edit-pwd").click(function () { 
+        location.href = 'setting.html';
+    });
+    // $("").click(function () { 
+    //     location.href = 'setting.html';
+    // });
 });
